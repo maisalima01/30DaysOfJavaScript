e187fcdf86ec9df60baa127aa515410ab697f69d
Maísa Lima <maisafernanda@hotmail.com>
2022-11-24 20:15:16 -0300

    01DayIntroduction

---
+++ 01_day_introduction/01_day_starter/variable.js
@@ -0,0 +1,14 @@
+let firstName = "Liz";
+let lastName = "Castilho";
+let country = "Brasil";
+let city = "São Paulo";
+let age = 3;
+let isMarried = false;
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+let name = "Liz",
+  job = "student",
+  live = "Brasil";
